cc7fab8833ce3d17ee4bb2da4d19931911c2c1a3
CloudbaseProjectApp <cloudbaseprojectapp@gmail.com>
2025-08-03 10:46:59 -0600

    Added detail colors to flying potential detail view

--- Cloudbase Project/FlyingPotentialDetailView.swift
+++ Cloudbase Project/FlyingPotentialDetailView.swift
@@ -4,10 +4,13 @@ import Combine
 struct FlyingPotentialDetailView: View {
     var site: Site                  // Received from parent view
     var favoriteName: String?       // Override display name if site detail is for a user favorite
-    var forecastData: ForecastData  // For a single hour for the site
-    var forecastIndex: Int
+    var forecastData: ForecastData  // Forecast by hour format
+    var forecastIndex: Int          // Index to use for correct date/hour
     
     @Environment(\.presentationMode) var presentationMode
+
+    let dataWidth: CGFloat = 44
+    let rowHeight: CGFloat = 32
 
     var body: some View {
         let displayName = favoriteName ?? site.siteName
@@ -51,37 +54,157 @@ struct FlyingPotentialDetailView: View {
                     .bold())
                 {
                     HStack {
-/*                        Text(forecastData.formattedDay? ?? "")
+                        Text("Details for: ")
                             .font(.caption)
-                            .frame(width: dataWidth)
-                            .padding(.top, 8)
-                        // Display divider when date changes
-                            .overlay ( Divider() .frame(width: dateChangeDividerSize, height: headingHeight) .background(getDividerColor(hourly.newDateFlag?[i] ?? true)), alignment: .leading )
-                        Text(hourly.formattedDate?[i] ?? "")
+                        Text(forecastData.hourly.formattedDay?[forecastIndex] ?? "")
                             .font(.caption)
-                            .frame(width: dataWidth)
- */
+                        Text(forecastData.hourly.formattedDate?[forecastIndex] ?? "")
+                            .font(.caption)
+                        Text(forecastData.hourly.formattedTime?[forecastIndex] ?? "")
+                            .font(.caption)
                     }
-                        Text("flying potential is based on....")
-                            .font(.footnote)
-                            .foregroundColor(infoFontColor)
+                    HStack {
+                        Text("Combined flying potential")
+                            .font(.caption)
+                        let displayColor = FlyingPotentialColor.color(for: forecastData.hourly.combinedColorValue![forecastIndex])
+                        let displaySize = FlyingPotentialImageSize(displayColor)
+                        Image(systemName: flyingPotentialImage)
+                            .resizable()
+                            .scaledToFit()
+                            .font(.system(size: displaySize))
+                            .frame(width: displaySize, height: displaySize)
+                            .foregroundColor(Color(displayColor))
+                            .padding(8)
+                            .frame(width: dataWidth, height: rowHeight)
+                    }
+                    HStack {
+                        Text("Cloud cover")
+                            .font(.caption)
+                        let displayColor = FlyingPotentialColor.color(for: forecastData.hourly.cloudCoverColorValue![forecastIndex])
+                        let displaySize = FlyingPotentialImageSize(displayColor)
+                        Image(systemName: flyingPotentialImage)
+                            .resizable()
+                            .scaledToFit()
+                            .font(.system(size: displaySize))
+                            .frame(width: displaySize, height: displaySize)
+                            .foregroundColor(Color(displayColor))
+                            .padding(8)
+                            .frame(width: dataWidth, height: rowHeight)
+                    }
+                    HStack {
+                        Text("Precipitation probability")
+                            .font(.caption)
+                        let displayColor = FlyingPotentialColor.color(for: forecastData.hourly.precipColorValue![forecastIndex])
+                        let displaySize = FlyingPotentialImageSize(displayColor)
+                        Image(systemName: flyingPotentialImage)
+                            .resizable()
+                            .scaledToFit()
+                            .font(.system(size: displaySize))
+                            .frame(width: displaySize, height: displaySize)
+                            .foregroundColor(Color(displayColor))
+                            .padding(8)
+                            .frame(width: dataWidth, height: rowHeight)
+                    }
+                    HStack {
+                        Text("CAPE")
+                            .font(.caption)
+                        let displayColor = FlyingPotentialColor.color(for: forecastData.hourly.CAPEColorValue![forecastIndex])
+                        let displaySize = FlyingPotentialImageSize(displayColor)
+                        Image(systemName: flyingPotentialImage)
+                            .resizable()
+                            .scaledToFit()
+                            .font(.system(size: displaySize))
+                            .frame(width: displaySize, height: displaySize)
+                            .foregroundColor(Color(displayColor))
+                            .padding(8)
+                            .frame(width: dataWidth, height: rowHeight)
+                    }
+                    HStack {
+                        Text("Wind Direction")
+                            .font(.caption)
+                        let displayColor = FlyingPotentialColor.color(for: forecastData.hourly.windDirectionColorValue![forecastIndex])
+                        let displaySize = FlyingPotentialImageSize(displayColor)
+                        Image(systemName: flyingPotentialImage)
+                            .resizable()
+                            .scaledToFit()
+                            .font(.system(size: displaySize))
+                            .frame(width: displaySize, height: displaySize)
+                            .foregroundColor(Color(displayColor))
+                            .padding(8)
+                            .frame(width: dataWidth, height: rowHeight)
+                    }
+                    HStack {
+                        Text("Surface wind speed")
+                            .font(.caption)
+                        let displayColor = FlyingPotentialColor.color(for: forecastData.hourly.surfaceWindColorValue![forecastIndex])
+                        let displaySize = FlyingPotentialImageSize(displayColor)
+                        Image(systemName: flyingPotentialImage)
+                            .resizable()
+                            .scaledToFit()
+                            .font(.system(size: displaySize))
+                            .frame(width: displaySize, height: displaySize)
+                            .foregroundColor(Color(displayColor))
+                            .padding(8)
+                            .frame(width: dataWidth, height: rowHeight)
+                    }
+                    HStack {
+                        Text("Surface gust speed")
+                            .font(.caption)
+                        let displayColor = FlyingPotentialColor.color(for: forecastData.hourly.surfaceGustColorValue![forecastIndex])
+                        let displaySize = FlyingPotentialImageSize(displayColor)
+                        Image(systemName: flyingPotentialImage)
+                            .resizable()
+                            .scaledToFit()
+                            .font(.system(size: displaySize))
+                            .frame(width: displaySize, height: displaySize)
+                            .foregroundColor(Color(displayColor))
+                            .padding(8)
+                            .frame(width: dataWidth, height: rowHeight)
+                    }
+                    HStack {
+                        Text("Gust factor")
+                            .font(.caption)
+                        let displayColor = FlyingPotentialColor.color(for: forecastData.hourly.gustFactorColorValue![forecastIndex])
+                        let displaySize = FlyingPotentialImageSize(displayColor)
+                        Image(systemName: flyingPotentialImage)
+                            .resizable()
+                            .scaledToFit()
+                            .font(.system(size: displaySize))
+                            .frame(width: displaySize, height: displaySize)
+                            .foregroundColor(Color(displayColor))
+                            .padding(8)
+                            .frame(width: dataWidth, height: rowHeight)
+                    }
+                    HStack {
+                        Text("Winds aloft speed")
+                            .font(.caption)
+                        let displayColor = FlyingPotentialColor.color(for: forecastData.hourly.windsAloftColorValue![forecastIndex])
+                        let displaySize = FlyingPotentialImageSize(displayColor)
+                        Image(systemName: flyingPotentialImage)
+                            .resizable()
+                            .scaledToFit()
+                            .font(.system(size: displaySize))
+                            .frame(width: displaySize, height: displaySize)
+                            .foregroundColor(Color(displayColor))
+                            .padding(8)
+                            .frame(width: dataWidth, height: rowHeight)
+                    }
+                    HStack {
+                        Text("Winds aloft speed")
+                            .font(.caption)
+                        let displayColor = FlyingPotentialColor.color(for: forecastData.hourly.thermalVelocityColorValue![forecastIndex])
+                        let displaySize = FlyingPotentialImageSize(displayColor)
+                        Image(systemName: flyingPotentialImage)
+                            .resizable()
+                            .scaledToFit()
+                            .font(.system(size: displaySize))
+                            .frame(width: displaySize, height: displaySize)
+                            .foregroundColor(Color(displayColor))
+                            .padding(8)
+                            .frame(width: dataWidth, height: rowHeight)
                     }
                 }
-
-                
-                
-/*                var combinedColorValue: [Int]?
-                var cloudCoverColorValue: [Int]?
-                var precipColorValue: [Int]?
-                var CAPEColorValue: [Int]?
-                var windDirectionColorValue: [Int]?
-                var surfaceWindColorValue: [Int]?
-                var surfaceGustColorValue: [Int]?
-                var gustFactorColorValue: [Int]?
-                var windsAloftColorValue: [Int]?
-                var thermalVelocityColorValue: [Int]?
- */
-
+            }
         }
     }
 }
